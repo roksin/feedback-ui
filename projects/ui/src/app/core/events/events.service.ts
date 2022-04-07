0556c6301fcf8624677f76322bfa8ccd0a5cac8f
Gökhan Akın <roksinmoruk@gmail.com>
2022-04-07 13:49:56 +0300

    event window

--- projects/ui/src/app/core/events/events.service.ts
+++ projects/ui/src/app/core/events/events.service.ts
@@ -6,37 +6,39 @@ import {FeedbackRequestModel} from '../../models/feedback-request.model';
 import {FeedbackService} from '../../services/feedback/feedback.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class EventsService {
 
-  private _modalShowButtonAction$: BehaviorSubject<null> = new BehaviorSubject<null>(null);
-  private _modalCloseButtonAction$: BehaviorSubject<null> = new BehaviorSubject<null>(null);
+    private _modalShowButtonAction$: BehaviorSubject<null> = new BehaviorSubject<null>(null);
+    private _modalCloseButtonAction$: BehaviorSubject<null> = new BehaviorSubject<null>(null);
 
-  constructor(private feedbackService: FeedbackService) {}
+    constructor(private feedbackService: FeedbackService) {
+    }
 
-  get modalShowButtonAction$(): Observable<any> {
-    return this._modalShowButtonAction$.asObservable();
-  }
+    get modalShowButtonAction$(): Observable<any> {
+        return this._modalShowButtonAction$.asObservable();
+    }
 
-  get modalCloseButtonAction$(): Observable<any> {
-    return this._modalCloseButtonAction$.asObservable();
-  }
+    get modalCloseButtonAction$(): Observable<any> {
+        return this._modalCloseButtonAction$.asObservable();
+    }
 
-  onShowModalButtonClick(): void {
-    this._modalShowButtonAction$.next(null);
-  }
+    onShowModalButtonClick(): void {
+        this._modalShowButtonAction$.next(null);
+        window.postMessage({sender: 'feedback', message: 'openModal'}, '*');
+    }
 
-  onSendFeedbackButtonClick(commentStr: string): void {
-    const feedbackReq = new FeedbackRequestModel();
-    feedbackReq.feedback = commentStr;
-    feedbackReq.companyId = 'try-1001';
-    this.feedbackService.createFeedback(feedbackReq).subscribe();
-  }
+    onSendFeedbackButtonClick(commentStr: string): void {
+        const feedbackReq = new FeedbackRequestModel();
+        feedbackReq.feedback = commentStr;
+        feedbackReq.companyId = 'try-1001';
+        this.feedbackService.createFeedback(feedbackReq).subscribe();
+    }
 
-  onCloseModalButtonClick(): void {
-    this.feedbackService._sendFeedbackSuccess.next({success: false});
-    this._modalCloseButtonAction$.next(null);
-  }
+    onCloseModalButtonClick(): void {
+        this.feedbackService._sendFeedbackSuccess.next({success: false});
+        this._modalCloseButtonAction$.next(null);
+    }
 }
 
